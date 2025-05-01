b6301e017c87e676f170116f3b7d838e444b36aa
Mimukauwa <solavolodimir@gmail.com>
2025-05-01 20:36:48 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,7 +1,7 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Секретная пизда вованчика",
-   Icon = "111001621656850", -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   Icon = 111001621656850, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Я люблю неко сик аххххх",
    LoadingSubtitle = "by неко сик",
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
