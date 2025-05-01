b43082232411e91d9b49d3d5024ef9ed962785d4
Mimukauwa <solavolodimir@gmail.com>
2025-05-01 17:16:33 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/jalilwas/Sirius-Plugins/refs/heads/main/Rayfield%20Logo%20Remover'))()
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Секретная пизда вованчика",
